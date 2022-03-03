7df961edd5bc247ac97e9576c4e6e4a2e20ca5e5
orellachichester@gmail.com <orellachichester@gmail.com>
2022-03-02 23:47:26 -0500

    Created get request to locate issue by id number

--- capstone/src/main/java/com/orella/capstone/controllers/HelpController.java
+++ capstone/src/main/java/com/orella/capstone/controllers/HelpController.java
@@ -3,10 +3,13 @@ package com.orella.capstone.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.orella.capstone.exceptions.ResourceNotFoundException;
 import com.orella.capstone.models.HelpModel;
 import com.orella.capstone.models.WishlistModel;
 import com.orella.capstone.repository.HelpRepository;
@@ -23,5 +26,14 @@ public class HelpController {
 		public List<HelpModel> getAllHelpIssues() {
 			return helpRepo.findAll();
 		}
-	
+		
+		
+		//Get request that finds issues on help table by their id number 
+		@GetMapping("/findissuebyid/{id}")
+		public ResponseEntity<HelpModel> getIssueById(@PathVariable int id){
+			HelpModel issue = helpRepo.findById(id)
+					.orElseThrow(() -> new ResourceNotFoundException ("Issue type not found."));
+					return ResponseEntity.ok(issue);
+		}
+		
 }
