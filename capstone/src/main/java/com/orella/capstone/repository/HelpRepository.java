74b8abbc3445df447dd33a0fb2b3505ed0649092
orellachichester@gmail.com <orellachichester@gmail.com>
2022-03-03 22:33:56 -0500

    Heroku commit

--- capstone/src/main/java/com/orella/capstone/repository/HelpRepository.java
+++ capstone/src/main/java/com/orella/capstone/repository/HelpRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.orella.capstone.models.HelpModel;
-import com.orella.capstone.models.WishlistModel;
+
 
 @Repository
 public interface HelpRepository extends JpaRepository <HelpModel, Integer> {
